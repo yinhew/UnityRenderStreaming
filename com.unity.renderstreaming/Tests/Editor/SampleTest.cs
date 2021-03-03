8edd2bd76ed271350e77e1f4e9f20a12754b5ea7
kazuki matsumoto <karasusan@gmail.com>
2021-03-03 15:05:57 +0900

    [skip ci] fix test

--- com.unity.renderstreaming/Tests/Editor/SampleTest.cs
+++ com.unity.renderstreaming/Tests/Editor/SampleTest.cs
@@ -1,18 +1,34 @@
+using System.Collections;
+using System.Linq;
 using NUnit.Framework;
+using UnityEditor.Compilation;
 using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Unity.RenderStreaming.EditorTest
 {
     class SampleTest
     {
-        [Test]
-        public void Import()
+        [UnityTest, Timeout(5000)]
+        public IEnumerator Import()
         {
             var samples = Sample.FindByPackage("com.unity.renderstreaming", "3.0.0-preview.1");
             foreach (var sample in samples)
             {
                 sample.Import(Sample.ImportOptions.OverridePreviousImports);
             }
+
+            bool completed = false;
+            CompilationPipeline.assemblyCompilationFinished += (s, messages) =>
+            {
+                var messageTypes= messages.Select(m => m.type);
+                Assert.That(messageTypes, Has.None.EqualTo(CompilerMessageType.Error));
+            };
+            CompilationPipeline.compilationFinished += o => { completed = true; };
+            CompilationPipeline.RequestScriptCompilation();
+
+            yield return new WaitUntil(() => completed);
         }
     }
 }
